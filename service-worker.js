380c0e27c2b7fd03a8c40d11b1be4cc90b004d59
Ishaan Joshi <121894115+IshaanJ2525@users.noreply.github.com>
2025-07-26 14:34:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
-const CACHE_NAME = 'workout-tracker-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/app.js',
-  '/manifest.json',
-  '/assets/icon.png'
+const CACHE_NAME = "gym-tracker-v1";
+const ASSETS_TO_CACHE = [
+  "./",
+  "index.html",
+  "style.css",
+  "app.js",
+  "manifest.json",
+  "assets/icon.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+  );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => response || fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() =>
+      caches.match(event.request).then(response => response)
+    )
   );
 });
